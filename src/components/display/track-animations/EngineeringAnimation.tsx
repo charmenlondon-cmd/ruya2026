cc679fd2e11b7d171b7907c422b70fce70a2f539
charmenlondon-cmd <charmenlondon@gmail.com>
2026-08-05 09:39:48 +0400

    fix(display): raise Engineering _2 animation to sit on question card

--- src/components/display/track-animations/EngineeringAnimation.tsx
+++ src/components/display/track-animations/EngineeringAnimation.tsx
@@ -30,7 +30,7 @@ export function EngineeringAnimation() {
           animationData={anim2}
           loop
           className="absolute opacity-80"
-          style={{ width: W, height: H2, bottom: 'calc(72% - 20px)', left: 'calc(50% - 512px)' }}
+          style={{ width: W, height: H2, bottom: '76%', left: 'calc(50% - 512px)' }}
         />
       )}
     </div>
